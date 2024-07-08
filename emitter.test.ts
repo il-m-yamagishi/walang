23308c999dcfb736c6601dd86c7c7439a8f257b4
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2024-07-08 17:22:49 +0900

    add GitHub Actions

--- emitter.test.ts
+++ emitter.test.ts
@@ -2,12 +2,12 @@ import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { emitter } from "./emitter.ts";
 
 Deno.test("emitter has magic number", () => {
-	const actual = emitter();
-	assert(actual instanceof Uint8Array);
-	assert(actual.length === 8);
+  const actual = emitter();
+  assert(actual instanceof Uint8Array);
+  assert(actual.length === 8);
 });
 
 Deno.test("emitter has valid wasm", async () => {
-	const wasm = emitter();
-	await WebAssembly.instantiate(wasm);
+  const wasm = emitter();
+  await WebAssembly.instantiate(wasm);
 });
--- emitter.ts
+++ emitter.ts
@@ -8,5 +8,5 @@ const MAGIC_MODULE_HEADER = [0x00, 0x61, 0x73, 0x6d];
 const MODULE_VERSION = [0x01, 0x00, 0x00, 0x00];
 
 export const emitter = () => {
-	return Uint8Array.from([...MAGIC_MODULE_HEADER, ...MODULE_VERSION]);
+  return Uint8Array.from([...MAGIC_MODULE_HEADER, ...MODULE_VERSION]);
 };
